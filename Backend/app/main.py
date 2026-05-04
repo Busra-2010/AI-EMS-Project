0b5ca43ad4fac39fb1e0956fde3b98e8a3fc60a2
Busra Aktar <rubidi8581@gmail.com>
2026-05-04 21:54:50 +0530

    fix CORS origins

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -17,8 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ai-ems-project.vercel.app/login",
-        "https://*.vercel.app", 
+        "https://ai-ems-project.vercel.app",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
